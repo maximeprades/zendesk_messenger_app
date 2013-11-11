1ddda1deb3894a87ae2ed6afc3a72e6f248edef7
Thomas Pelletier <pelletier.thomas@gmail.com>
2013-11-10 19:38:26 -0800

    Add messenging

--- app.js
+++ app.js
@@ -1,41 +1,89 @@
 (function() {
 
   return {
+    defaultState: 'main',
+
     events: {
-      'app.activated'     :  'init',
-      'activeUsers.done'  :  'switchToUserList',
-      'activeUsers.fail'  :  'switchToError'
+      'app.activated'   : 'init',
+     'pane.activated'  : 'onPaneActivated',
+      'activeUsers.done': 'onActiveUsersDone',
+      'activeUsers.fail': 'switchToError',
+      'click #send-msg' : 'sendMessage',
+      'notification.message': 'onIncomingMessage'
     },
 
     requests: {
       presence: function() {
         return this._request('/presence/%@'.fmt(this.currentUser().id()), 'POST');
       },
-
       activeUsers: function() {
         return this._request('/presence/active_users');
+      },
+      sendMessage: function(to, message) {
+        return {
+          url: '/api/v2/apps/notify',
+          type: 'POST',
+          data: {
+            event: 'message',
+            body: {
+              text: message,
+              from: this.currentUser().name(),
+              to: to
+            },
+            app_id: 0
+          }
+        };
       }
     },
+
+    storage: {},
 
     init: function() {
       this.sayPresence();
     },
 
-    sayPresence: function() {
-      this.ajax('presence');
+    onPaneActivated: function() {
       this.ajax('activeUsers');
-
-      setInterval(this.sayPresence.bind(this), 1000 * 60);
     },
 
-    switchToUserList: function(data) {
-      this.switchTo('userlist', {
-        activeUsers: data.active_ids
-      });
+    drawActiveUsers: function() {
+      this.$('.activeUsers').html(this.renderTemplate('userlist', {
+        activeUsers: this.storage.activeUsers
+      }));
+    },
+
+    sayPresence: function() {
+      this.when(this.ajax('presence'), this.ajax('activeUsers')).then(function() {
+        setInterval(this.sayPresence.bind(this), 1000 * 60);
+      }.bind(this));
+    },
+
+    onActiveUsersDone: function(data) {
+      this.storage.activeUsers = data.active_ids;
+      this.drawActiveUsers();
     },
 
     switchToError: function() {
       this.switchTo('error');
+    },
+
+    sendMessage: function() {
+      var area = this.$('.message');
+      var message = area.val();
+      var to = this.$('.activeUsers').val();
+      this.ajax('sendMessage', to, message);
+      area.val('');
+    },
+
+    onIncomingMessage: function(data) {
+      if (data.to === this.currentUser().id()) {
+        this.popover();
+        _.defer(this.drawMessage.bind(this), data);
+      }
+    },
+
+    drawMessage: function(message) {
+      this.$('#messages').append(this.renderTemplate('message', message));
     },
 
     _request: function(url, type, params) {
@@ -48,6 +96,5 @@
         data: data
       };
     }
-
   };
 }());
