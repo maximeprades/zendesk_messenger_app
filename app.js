f44395478507784e3de78454c235d3bf8030cb65
Maxime <mprades@zendesk.com>
2013-11-10 18:34:06 -0800

    adding users polling

--- app.js
+++ app.js
@@ -14,13 +14,25 @@
             secret: this.setting('messenger_secret')
           }
         };
+      },
+
+      activeUsers: function() {
+        return {
+          url: "https://zendesk-messenger.herokuapp.com/account/" + this.currentAccount().subdomain() + "/presence/active_users",
+          type: 'GET',
+          data: {
+            secret: this.setting('messenger_secret')
+          }
+        };
       }
     },
 
     sayPresence: function() {
       this.ajax('presence');
+      this.ajax('activeUsers');
       setInterval (function() {
         this.ajax('presence');
+        this.ajax('activeUsers');
     }.bind(this),1000*60);
    }
   };
