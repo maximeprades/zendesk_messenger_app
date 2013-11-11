6e7e897c811bd7f02b908361f0a2f8f158aad752
Thomas Pelletier <pelletier.thomas@gmail.com>
2013-11-10 19:03:03 -0800

    refactor

--- app.js
+++ app.js
@@ -2,51 +2,51 @@
 
   return {
     events: {
-      'app.activated'     :  'sayPresence',
+      'app.activated'     :  'init',
       'activeUsers.done'  :  'switchToUserList',
       'activeUsers.fail'  :  'switchToError'
     },
 
     requests: {
       presence: function() {
-        return {
-          url: "https://zendesk-messenger.herokuapp.com/account/" + this.currentAccount().subdomain() + "/presence/" + this.currentUser().id(),
-          type: 'POST',
-          data: {
-            secret: this.setting('messenger_secret')
-          }
-        };
+        return this._request('/presence/%@'.fmt(this.currentUser().id()), 'POST');
       },
 
       activeUsers: function() {
-        return {
-          url: "https://zendesk-messenger.herokuapp.com/account/" + this.currentAccount().subdomain() + "/presence/active_users",
-          type: 'GET',
-          data: {
-            secret: this.setting('messenger_secret')
-          }
-        };
+        return this._request('/presence/active_users');
       }
+    },
+
+    init: function() {
+      this.sayPresence();
     },
 
     sayPresence: function() {
       this.ajax('presence');
       this.ajax('activeUsers');
-      setInterval (function() {
-        this.ajax('presence');
-        this.ajax('activeUsers');
-      }.bind(this),1000*60);
+
+      setInterval(this.sayPresence.bind(this), 1000 * 60);
     },
 
-  switchToUserList: function(data) {
-     this.switchTo('userlist',
-      {
+    switchToUserList: function(data) {
+      this.switchTo('userlist', {
         activeUsers: data.active_ids
       });
     },
 
-  switchToError: function() {
-     this.switchTo('error');
+    switchToError: function() {
+      this.switchTo('error');
+    },
+
+    _request: function(url, type, params) {
+      var data = _.extend({
+        secret: this.setting('messenger_secret')
+      }, params);
+      return {
+        url: helpers.fmt("https://zendesk-messenger.herokuapp.com/account/%@%@", this.currentAccount().subdomain(), url),
+        type: type,
+        data: data
+      };
     }
 
   };
